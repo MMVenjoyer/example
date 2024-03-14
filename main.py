5aa49fcf3b128a20669638431d20af70f38a7846
MMVenjoyer <mmvenjoyer@gmail.com>
2024-03-14 17:53:35 +0500

    Глобальные улучшения
    Исправил локальный handler, работающий по цифрам на FSM машину, улучшил читаемость, пофиксил баги

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import markups as kb
 import aiohttp
 
 from datetime import datetime
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from db import DataManipulation
@@ -16,13 +18,18 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-async def get_data_by_article(product_article: int):
+class UserState(StatesGroup):
+    product_article = State()
+
+
+async def get_data_by_article(product_article: int) -> (tuple[str, int, int, float, int]):
 	'''Получаем данные по артиклю и конвертированные кортежем возвращаем назад'''
 	async with aiohttp.ClientSession() as session:
 		async with session.get(f'https://card.wb.ru/cards/v1/detail?appType=1&curr=rub&dest=-1257786&spp=30&nm={product_article}') as result:
-			result = await result.json()
-			if result: # Проверяем успешен ли запрос
-				result = result['data']['products'][0]
+			result_status = result.status
+			if result_status == 200:
+				result = await result.json()
+				result = result['data']['products'][0] # тут есть уязвимость на артикул 000000000000000000000000
 				product_name = result['name']
 				product_article = result['id']
 				product_price = result['salePriceU']
@@ -30,14 +37,14 @@ async def get_data_by_article(product_article: int):
 				try:
 					product_last = result['sizes'][0]['stocks'][0]['qty']
 				except:
-					product_last = 'Нет в наличии'
+					product_last = 0
 
 				return (product_name, product_article, product_price, product_rate, product_last,)
 			else:
 				return False
 
 
-async def get_generated_answer(text_to_generate: tuple, status: bool):
+async def get_generated_answer(text_to_generate: tuple, status: bool) -> str:
 	'''Генерирует ответ по необходимости и в взависимости от возможного количества строк в бд(не больше 5)'''
 	if status:
 		return f"""
@@ -67,25 +74,14 @@ ID: {text_to_generate[i].user_id}
 
 @dp.message_handler(commands = ['start'])
 async def get_start(message: types.Message):
-	await bot.send_message(message.from_user.id, 'Старт', reply_markup=kb.kb_menu)
+	await bot.send_message(message.from_user.id, 'Бот для примера кода', reply_markup=kb.kb_menu)
 
 
 @dp.message_handler(text = ['Получить информацию по товару✳️', 'Получить информацию из БД⏺'])
 async def get_main_menu(message: types.Message):
 	if message.text == 'Получить информацию по товару✳️': # Получаем инфу по артиклю
 		await bot.send_message(message.from_user.id, 'Отправьте мне нужный вам артикул:')
-		@dp.message_handler(lambda message: message.text.isdigit())
-		async def get_product_info(message: types.Message):
-			result = await asyncio.create_task(get_data_by_article(int(message.text)))
-			if result: # Проверяем успешен ли запрос
-				try:
-					DataManipulation.write_data(int(message.from_user.id), str(datetime.now()), int(result[1]))
-				except:
-					print('ошибка добавления')
-				answer_text = await asyncio.create_task(get_generated_answer(result, True))
-				await bot.send_message(message.from_user.id, answer_text)
-			else:
-				await bot.send_message(message.from_user.id, "Возникла ошибка!")
+		await UserState.product_article.set()
 
 	elif message.text == 'Получить информацию из БД⏺': # Получаем 5 последних запросов
 		result = DataManipulation.get_last_requests(message.from_user.id, 5)
@@ -94,6 +90,34 @@ async def get_main_menu(message: types.Message):
 		else:
 			answer_text = await asyncio.create_task(get_generated_answer(result, False))
 			await bot.send_message(message.from_user.id, answer_text)
+
+
+####################################################################
+# 							ХЭНДЛЕРЫ FSM						   #
+####################################################################
+			
+
+@dp.message_handler(state=UserState.product_article)
+async def get_product_article(message: types.Message, state: FSMContext):
+	await state.update_data(product_article=message.text)
+	state_data = await state.get_data()
+	product_article = state_data['product_article']
+	if product_article.isdigit() and 7 < len(str(product_article)) < 14:
+		result = await asyncio.create_task(get_data_by_article(int(product_article)))
+		if result:
+			try:
+				DataManipulation.write_data(int(message.from_user.id), str(datetime.now()), int(result[1]))
+			except:
+				print('ошибка добавления')
+			answer_text = await asyncio.create_task(get_generated_answer(result, True))
+			await bot.send_message(message.from_user.id, answer_text)
+		else:
+			await bot.send_message(message.from_user.id, "Возникла ошибка на стороне сервера!")
+	else:
+		await bot.send_message(message.from_user.id, "Вы ввели неправильный артикул!")
+
+	await state.finish()
+
 
 
 ######################################################################################################################################
